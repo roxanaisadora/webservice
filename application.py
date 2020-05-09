e68599ddf8550ecad67b3bfdc00f16097e5fda92
Roxana Isadora <roxanaisadora.gmail.com>
2020-05-08 23:25:17 -0500

    cambiando a la estructura formal

--- application.py
+++ application.py
@@ -1,32 +1,25 @@
 
-# Módulos creados
-#from server import *
-#from models import *
-#from controllers.controllerUsuario import UsuarioController, UsuarioPostController, EstadoController, UsuarioNombreController, UsuarioMostraidController, UsuarioMostrarestadoController
-#from controllers.userController import UserPostController
-#from controllers.loginController import LoginController
-  
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-  return 'Hey its Python Flask application!'
+#Módulos creados
+from server import *
+from models import *
+from controllers.controllerUsuario import UsuarioController, UsuarioPostController, EstadoController, UsuarioNombreController, UsuarioMostraidController, UsuarioMostrarestadoController
+from controllers.userController import UserPostController
+from controllers.loginController import LoginController
 
 
-#usuario = api.namespace('api', description='Usuario API')
-#usuario.add_resource(UsuarioPostController, '/usuario')
-#usuario.add_resource(UsuarioController, '/usuario/<int:id>')
-#usuario.add_resource(EstadoController, '/usuario/estado/<int:id>')
-#usuario.add_resource(UsuarioMostrarestadoController, '/usuario/estado')
-#usuario.add_resource(UsuarioNombreController, '/usuario/busqueda')
-#usuario.add_resource(UsuarioMostraidController, '/usuario/iddata/<int:id>')
+usuario = api.namespace('api', description='Usuario API')
+usuario.add_resource(UsuarioPostController, '/usuario')
+usuario.add_resource(UsuarioController, '/usuario/<int:id>')
+usuario.add_resource(EstadoController, '/usuario/estado/<int:id>')
+usuario.add_resource(UsuarioMostrarestadoController, '/usuario/estado')
+usuario.add_resource(UsuarioNombreController, '/usuario/busqueda')
+usuario.add_resource(UsuarioMostraidController, '/usuario/iddata/<int:id>')
 
-#user = api.namespace('api', description='User API')
-#user.add_resource(UserPostController, '/user')
+user = api.namespace('api', description='User API')
+user.add_resource(UserPostController, '/user')
 
-#login = api.namespace('api', description='Login API')
-#login.add_resource(LoginController, '/login')
+login = api.namespace('api', description='Login API')
+login.add_resource(LoginController, '/login')
 
 
 if __name__ == "__main__":
